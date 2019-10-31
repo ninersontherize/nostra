54ad6d5521b6d8dcfb2e3b64314eca65469d42f1
Ryan McNicol <rmcnicol@alg.com>
2019-10-31 15:47:21 -0700

    deployment cleanup

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const passport = require("passport")
+const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/users");
 
@@ -20,16 +21,23 @@ const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
+  .connect(db, { useNewUrlParser: true })
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
 
 app.use(passport.initialize());
 require("./config/passport")(passport);
 app.use("/api/users", users);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 // this is heroku's port when we deploy there
