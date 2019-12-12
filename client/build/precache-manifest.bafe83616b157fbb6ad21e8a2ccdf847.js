ad2292ce69472b0957d31151f4f645f804e338b1
Ryan McNicol <rmcnicol@alg.com>
2019-12-12 13:54:13 -0800

    adding build folder

---
+++ client/build/precache-manifest.bafe83616b157fbb6ad21e8a2ccdf847.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f00455b6feca457d9a9c93f4298ee8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf22c72cc740b82be753",
+    "url": "/static/css/2.7d36abf2.chunk.css"
+  },
+  {
+    "revision": "4469ea8c7e4226c21e02",
+    "url": "/static/css/main.39f42490.chunk.css"
+  },
+  {
+    "revision": "bf22c72cc740b82be753",
+    "url": "/static/js/2.ec4c4626.chunk.js"
+  },
+  {
+    "revision": "4469ea8c7e4226c21e02",
+    "url": "/static/js/main.8ebb22c3.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
