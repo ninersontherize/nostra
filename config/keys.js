d537a4df5b8f6c58ccafc8c6490d5c698de80631
Ryan McNicol <rmcnicol@alg.com>
2020-01-09 13:49:37 -0800

    adding prod db string

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,7 @@
 module.exports = {
-  mongoURI: "mongodb+srv://ryanmcnicol:Passw0rd1@fantasy-pick-em-vjumb.mongodb.net/test?retryWrites=true&w=majority",
+  //dev server
+  //mongoURI: "mongodb+srv://ryanmcnicol:Passw0rd1@fantasy-pick-em-vjumb.mongodb.net/test?retryWrites=true&w=majority",
+  //prod server
+  mongoURI: "mongodb+srv://nostra_admin:D5C2AWwsu%23VxCEw@nostraproduction-zyeo9.mongodb.net/test?retryWrites=true&w=majority",
   secretOrKey: "secret"
 };
